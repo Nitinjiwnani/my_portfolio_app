69c1bec0e20a56c27570714bb0c6c72c288b3425
Nitinjiwnani <jiwnani01@gmail.com>
2022-07-16 00:07:30 +0530

    Basic Designing done

--- lib/main.dart
+++ lib/main.dart
@@ -20,14 +20,119 @@ class Home extends StatelessWidget {
               CircleAvatar(
                 radius: 40,
               ),
+              SizedBox(
+                width: 50,
+              ),
               Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text("Name"),
-                  Text("Designation"),
+                  Text(
+                    "Nitin jiwnani",
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  Text(
+                    "Designation",
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
                 ],
               )
             ],
-          )
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 30),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.school,
+                      size: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "School Name",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.computer,
+                      size: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      "School Name",
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    )
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.location_pin,
+                      size: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text("School Name",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      size: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text("School Name",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      size: 40,
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text("School Name",
+                        style: TextStyle(
+                          fontSize: 20,
+                        ))
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Text("About me"),
+          Text("Created By Me")
         ]),
       ),
     );
